b52f5a7284239e390b3bc880062d174f932a9d3a
Git-dev017 <vladislav.zilkov@gmail.com>
2017-04-08 14:43:19 +0300

    A&B are working

--- 3lab.c
+++ 3lab.c
@@ -1,22 +1,34 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 int main()
 {
-int pipe_descriptor[2];//0 - read 1 - write
-pipe(pipe_descriptor);
-int fork_result;
-char symbol;
-fork_result = fork();
-    if(fork_result == -1)
-        exit(EXIT_FAILURE);
-    if(fork_result == 0){ //child process
-        close(pipe_descriptor[1]);
-        read(pipe_descriptor[0], &symbol, pipe_descriptor);
-    }
-    else{//parent process
-        close(pipe_descriptor[0]);
-        write(pipe_descriptor[1], &symbol, pipe_descriptor);
-    }
+	int pipe_descriptor[2];//0 - read 1 - write
+	int fork_result;
+	unsigned int symbol=0;
+
+	pipe(pipe_descriptor);
+
+	fork_result = fork();
+    	if(fork_result == -1) exit(EXIT_FAILURE);
+   	while(1)
+	{
+		if(fork_result == 0)
+	{ //child process
+        	close(pipe_descriptor[1]);
+		read(pipe_descriptor[0], &symbol, sizeof(symbol));
+		symbol *= symbol;
+		printf("Child proc %d\n", symbol);
+    	}
+   	else
+	{//parent process
+        	close(pipe_descriptor[0]);
+		printf("Parent proc\n");
+		scanf("%d",&symbol);
+        	write(pipe_descriptor[1], &symbol, sizeof(symbol));
+    	}
+}
 return 0;
 }
