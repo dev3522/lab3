6b1b5b670a195225007daed8848d3ce7208d8daf
dev3522 <vladislav.zilkov@gmail.com>
2017-04-10 13:09:37 +0300

    added prinf

--- 3lab.c
+++ 3lab.c
@@ -1,10 +1,14 @@
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/shm.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 int main()
-{//
+{
 	int pipe_descriptor[2];//0 - read 1 - write
 	int fork_result;
 	unsigned int symbol=0;
@@ -21,7 +25,21 @@ int main()
 		read(pipe_descriptor[0], &symbol, sizeof(symbol));
 		symbol *= symbol;
 		printf("Child proc %d\n", symbol);
-    	}
+		int c_id=0;
+		const char name;
+		c_id = fork();
+		if(c_id < 0) exit(EXIT_FAILURE);
+                if(c_id == 0)
+                {
+                    printf("Entered");
+		int shm_variable=0; 
+		shm_variable = shm_open(&name, O_CREAT, RWX);
+                printf("\nshm_variable = %i", shm_variable);
+                if(shm_variable == 0){
+
+                }
+                        }
+    }
    	else
 	{//parent process
         	close(pipe_descriptor[0]);
