56247853c8d53f8aac01dae31b03f7da7d98d379
dev3522 <vladislav.zilkov@gmail.com>
2017-04-11 22:28:53 +0300

    ...

--- 3lab.c
+++ 3lab.c
@@ -26,38 +26,35 @@ int main()
         	close(pipe_descriptor[1]);
 		read(pipe_descriptor[0], &symbol, sizeof(symbol));
 		symbol *= symbol;
-		printf("Child proc %d\n", symbol);
-                int proc_c_id = 0;
+		printf("Child proc (B) %d\n", symbol);
+                int proc_c_id = 0, segment_id;
                 proc_c_id = fork();
                 if(proc_c_id < 0)
 		{ 
                         printf("proc_c_id<0\n");
 			exit(EXIT_FAILURE);
 		}
-                if(proc_c_id == 0)
+                if(proc_c_id == 0)//child process C
                 {
-                    printf("Entered\n");
-			key_t shmKEY;
-			shmKEY = 1111;//ftok(".", 'x');
-			printf("shmKEY = %i\n",shmKEY);
-		int segment_id;
-		const char name = 120;
-		segment_id = shmget(shmKEY, sizeof(int), IPC_CREAT|0666);
-		printf("Errno=%d\n",errno);
-                printf("segment_id = %i\n", segment_id);
+			printf("child process\n");
+                   
                 if(segment_id == -1){
 			printf("segment_id = -1\n");
 			exit(EXIT_FAILURE);
 		}
+
 		if(segment_id == 0){
 			printf("\nsegment_id = 0");
                 }
-
                 else{
-                    printf("\nMemory created");
+                    printf("\nMemory created\n");
+		key_t shmKEY=1;
+		printf("shmKEY = %d\n",shmKEY);		
+		segment_id = shmget(shmKEY, sizeof(int), IPC_CREAT|0666);
+		printf("Errno=%d\n",errno);
+                printf("segment_id = %i\n", segment_id);
                 }
-    }
-                }
+          }
    	else
 	{//parent process
         	close(pipe_descriptor[0]);
